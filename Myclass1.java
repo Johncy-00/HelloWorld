2be8a2cd6c45d21f0f7570e4e668e73947b70d4d
Johncy-00 <121791489+Johncy-00@users.noreply.github.com>
2023-01-02 14:31:12 +0530

    Add files via upload

---
+++ Myclass1.java
@@ -0,0 +1,17 @@
+package myprojectDelete;
+
+public class Myclass1 {
+
+	public void MyMethod()
+	{
+		System.out.println("I am in my method");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+		System.out.println("I am Johncy");
+		Myclass1 m= new Myclass1();
+		m.MyMethod();
+
+	}
+
+}
